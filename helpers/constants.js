8b13300bf5184130cf584bdbbfe1c6b93c8fdb98
Aleksandr Kolot <aleksandrkolot7@gmail.com>
2021-06-12 12:32:26 +0300

    helpers fix

---
+++ helpers/constants.js
@@ -0,0 +1,23 @@
+const Gender = {
+    MALE: 'male',
+    FEMALE: 'female',
+    NONE: 'none',
+
+}
+
+const HttpCode = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST:401,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    INTERNAL_SERVER_ERROR: 500,
+};
+
+module.exports = {
+    Gender,
+    HttpCode,
+}
